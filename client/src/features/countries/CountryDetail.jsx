9df38bd23484f4dc76864ab19f20f50276e51d27
filipslunsky <fslunsky@gmail.com>
2024-12-15 11:10:29 +0100

    added border countries, fixed bug for missing object props in CountryDetail.jsx

--- client/src/features/countries/CountryDetail.jsx
+++ client/src/features/countries/CountryDetail.jsx
@@ -12,8 +12,28 @@ const CountryDetail = () => {
     console.log(countryCode);
     console.log(country);
 
+    const getNeighbors = () => {
+        if (!country.borders) return [];
+        const borders = country.borders;
+        let neighbors = [];
+        for (let border of borders) {
+            let neighbor = countries.filter(item => item.alpha3Code === border)[0];
+            neighbors.push(neighbor)
+        }
+        return neighbors;
+    };
+
+    const neighbors = getNeighbors();
+
+    console.log(neighbors);
+    
+
     const navigateBack = () => {
         navigate('/');
+    };
+
+    const navigateToNeighbor = (countryCode) => {
+        navigate(`/country/${countryCode}`);
     };
 
     return (
@@ -30,11 +50,27 @@ const CountryDetail = () => {
                             <p className="detailInfoItem">Native Name: <span className="detailInfoValue">{country.nativeName}</span></p>
                             <p className="detailInfoItem">Population: <span className="detailInfoValue">{country.population}</span></p>
                             <p className="detailInfoItem">Region: <span className="detailInfoValue">{country.region}</span></p>
-                            <p className="detailInfoItem">Sub Region: <span className="detailInfoValue">{country.subregion}</span></p>
-                            <p className="detailInfoItem">Capital: <span className="detailInfoValue">{country.capital}</span></p>
-                            <p className="detailInfoItem">Top Level Domain: <span className="detailInfoValue">{country.topLevelDomain.join(', ')}</span></p>
-                            <p className="detailInfoItem">Currencies: <span className="detailInfoValue">{country.currencies.map(currency => currency.name).join(', ')}</span></p>
-                            <p className="detailInfoItem">Languages: <span className="detailInfoValue">{country.languages.map(language => language.name).join(', ')}</span></p>
+                            <p className="detailInfoItem">Sub Region: <span className="detailInfoValue">{country.subregion ? country.subregion : 'no sub region'}</span></p>
+                            <p className="detailInfoItem">Capital: <span className="detailInfoValue">{country.capital && country.capital > 0 ? country.capital : 'no capital'}</span></p>
+                            <p className="detailInfoItem">Top Level Domain: <span className="detailInfoValue">{country.topLevelDomain ? country.topLevelDomain.join(', ') : 'no domain'}</span></p>
+                            <p className="detailInfoItem">Currencies: <span className="detailInfoValue">{country.currencies ? country.currencies.map(currency => currency.name).join(', ') : 'no currency'}</span></p>
+                            <p className="detailInfoItem">Languages: <span className="detailInfoValue">{country.languages ? country.languages.map(language => language.name).join(', ') : 'no language'}</span></p>
+                        </div>
+                        <div className="neigborsContainer">
+                            <p className="detailInfoItem">Border Countries: {neighbors.length === 0 ? 'no border countries' : ''}</p>
+                            {
+                                neighbors.map(item => {
+                                    return (
+                                        <div
+                                        className="neighborItem"
+                                        key={item.alpha3Code}
+                                        onClick={() => {navigateToNeighbor(item.alpha3Code)}}
+                                        >
+                                            {item.name}
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
                 </div>
